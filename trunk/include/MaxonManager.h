9dd4acf1c624a16d66c521aba5e8b41edc1558cb
jonjake.anderson@gmail.com <jonjake.anderson@gmail.com@eac88406-8217-56cb-f4a2-964e7c8e07d6>
2011-10-04 12:42:57 +0000

    initial import

---
+++ trunk/include/MaxonManager.h
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : MaxonManager.h
+// Author      : Jon Anderson jonjake
+// Version     : 1.0
+// Date		   : Oct 2, 2011
+// Copyright   : Worcester Polytechnic Institute
+// Project	   : Oryx 2.0 (www.wpirover.com)
+// Description :
+//============================================================================
+
+#ifndef MAXONMANAGER_H_
+#define MAXONMANAGER_H_
+
+#include <iostream>
+#include <string>
+#include "MaxonMotor.h"
+
+using namespace std;
+
+unsigned int getNumberOfMotors();
+
+#endif /* MAXONMANAGER_H_ */
